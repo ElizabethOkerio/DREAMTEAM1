105ac576b6e1fd353a5ae7ff2e0fb5fb2c9083b1
Jonathan Kamau <kamaujonathan@gmail.com>
2017-02-10 15:49:14 +0300

    Update learning_map.py

--- learning_map.py
+++ learning_map.py
@@ -1,1 +1,51 @@
+import sqlite3
 
+
+"""db = sqlite3.connect('skills.db')
+cursor = db.cursor()
+cursor.execute('''
+    CREATE TABLE skills(id INTEGER PRIMARY KEY, skill_name TEXT,
+                       skill_status TEXT)
+''')
+db.commit()
+db.close()
+cursor = db.cursor()
+skill_name = 'Coding'
+skill_status= 'skilled'"""
+db = sqlite3.connect('skills.db')
+skill_name = var = raw_input("Enter Skill: ")
+skill_status = ""
+cursor = db.cursor()
+cursor.execute('''INSERT INTO skills(skill_name, skill_status)
+                  VALUES(?,?)''', (skill_name, skill_status))
+
+skill_name = var = raw_input("Press enter to view skills: ")
+
+
+db1 = cursor.execute('''SELECT id, skill_name FROM skills''')
+for row in cursor:
+   print ("ID = ", row[0])
+   print ("SKILL NAME = ", row[1])
+   
+
+"""db1 = cursor.execute("SELECT id, skill_name,skill_status from skills")
+for row in db1:
+   print ("ID = ", row[0])
+   print ("SKILL NAME = ", row[1])
+   print ("SKILL STATUS = ", row[2])"""
+
+skill_status = var = raw_input("Enter Status(Whether Studied or Not Studied): ")
+
+
+db1 = cursor.execute('''UPDATE skills SET skill_status = ? WHERE skill_name = ? ''',
+ (skill_status, skill_name))
+
+skill_name = var = raw_input("Press enter to view skills with their status: ")
+
+db1 = cursor.execute('''SELECT id, skill_name, skill_status FROM skills''')
+for row in cursor:
+   print ("ID = ", row[0])
+   print ("SKILL NAME = ", row[1])
+   print ("SKILL STATUS = ", row[2])
+
+db.close()
