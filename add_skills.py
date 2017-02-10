2a4add64111e87434d1a8848ebb3f400a97cbaeb
Lincoln Cmba <lincolncmba@gmail.com>
2017-02-10 15:46:39 +0300

    Update add_skills.py

--- add_skills.py
+++ add_skills.py
@@ -1,15 +1,12 @@
 import sqlite3
 
 conn = sqlite3.connect('skills.db')
-print "Opened database successfully";
-
 
 cursor = conn.execute("SELECT id, skill_name, skill_status  from skills")
 for row in cursor:
 	if row[2] == 'studied':
-		print "you've studied ", row[1]
+		print "you've studied ", row[1];
 	else: 
-		print "You've not studied", row[1]
+		print "You've not studied", row[1];
 		
-print "Operation done successfully";
 conn.close()
