0811bf8328607214dbb89b23a446d2441d9acd09
Jonathan Kamau <kamaujonathan@gmail.com>
2017-02-10 17:13:54 +0300

    Update learning_map.py

--- learning_map.py
+++ learning_map.py
@@ -13,13 +13,13 @@ cursor = db.cursor()
 skill_name = 'Coding'
 skill_status= 'skilled'"""
 db = sqlite3.connect('skills.db')
-skill_name = var = raw_input("Enter Skill: ")
-skill_status = ""
+skill_name = raw_input("Enter Skill: ")
+skill_status = raw_input("Enter Status(Whether Studied or Not Studied): ")
 cursor = db.cursor()
 cursor.execute('''INSERT INTO skills(skill_name, skill_status)
                   VALUES(?,?)''', (skill_name, skill_status))
 
-skill_name = var = raw_input("Press enter to view skills: ")
+
 
 
 db1 = cursor.execute('''SELECT id, skill_name FROM skills''')
@@ -34,13 +34,13 @@ for row in db1:
    print ("SKILL NAME = ", row[1])
    print ("SKILL STATUS = ", row[2])"""
 
-skill_status = var = raw_input("Enter Status(Whether Studied or Not Studied): ")
+
 
 
 db1 = cursor.execute('''UPDATE skills SET skill_status = ? WHERE skill_name = ? ''',
  (skill_status, skill_name))
 
-skill_name = var = raw_input("Press enter to view skills with their status: ")
+skill_name = raw_input("Press enter to view skills with their status: ")
 
 db1 = cursor.execute('''SELECT id, skill_name, skill_status FROM skills''')
 for row in cursor:
@@ -48,11 +48,11 @@ for row in cursor:
    print ("SKILL NAME = ", row[1])
    print ("SKILL STATUS = ", row[2])
 
-skill_name = var = raw_input("Press enter to view if skills are studied or not studied: ")
+skill_name = raw_input("Press enter to view if skills are studied or not studied: ")
 
 db1 = cursor.execute("SELECT id, skill_name, skill_status  from skills")
-for row in cursor:
-    if row[2] == 'studied':
+for row in db1:
+    if row[2] == "studied":
         print "you've studied ", row[1]
     else:
         print "You've not studied", row[1]            
